d987e425f4e888806d580f3d72e5e83f59ff57b3
Julio Edgar Mejia Rojas <julioedgarmejiarojas338@gmail.com>
2017-08-21 00:15:41 -0500

    Add estadoConverter Converter

---
+++ src/main/java/pe/cibertec/converter/EstadoConverter.java
@@ -0,0 +1,38 @@
+package pe.cibertec.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter("estadoConverter")
+public class EstadoConverter implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		return value;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		
+		String tipo = "";
+		
+		if(value != null) {
+			tipo = String.valueOf(value);
+			switch (tipo) {
+			case "1":
+				tipo = "Activo";
+				break;
+			case "0":
+				tipo = "INACTIVO";
+				break;
+			default:
+				tipo = "NO VALIDO";
+				break;
+			}
+		}
+		return tipo;
+	}
+
+}
